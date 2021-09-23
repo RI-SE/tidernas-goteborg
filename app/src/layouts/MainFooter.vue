f0d87218e697fc5685e4c3ddb0c72f54ec96a5d8
Martin Törnros <martin.tornros@ri.se>
2021-09-23 13:21:06 +0200

    Footer

---
+++ app/src/layouts/MainFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    https://itch.io/jam/tidernas-goteborg-400ars-jam
+  </div>
+</template>
+
+<script>
+
+export default ({
+  name: 'MainFooter',
+  data: function () {
+    return {
+    }
+  },
+  props: {
+  },
+  computed: {
+  },
+  methods: {
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
