f0604d81073ea780e6447517ab243a303ec2a2b4
Martin Törnros <martin.tornros@ri.se>
2021-09-27 11:57:12 +0200

    Changed example query

--- app/src/pages/PageAbout.vue
+++ app/src/pages/PageAbout.vue
@@ -9,7 +9,7 @@
         Det innebär att du kan söka efter bilder utifrån själva <i>innehållet</i> i bilderna.
       </p>
       <p>
-        Du kan till exempel hitta bilder på en <router-link :to="{name: 'Home', query: {q: 'grön fisk'}}">grön fisk</router-link> även om ingen människa har angett att någon bilderna innehåller just detta.
+        Du kan till exempel hitta bilder på en <router-link :to="{name: 'Home', query: {q: 'himmel'}}">himmel</router-link> även om ingen människa har angett att någon bilderna innehåller just detta.
       </p>
     </SectionRow>
   </q-page>
